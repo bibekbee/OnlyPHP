e4f56a9f574d55fa3de668447dc23d65e5660531
bibekbee <bibekbee23@gmail.com>
2024-05-29 09:28:40 +0545

    Database class to fetch data

--- database.php
+++ database.php
@@ -1,27 +1,42 @@
 <?php
 
-$dsn = "mysql:host=localhost;dbname=myapp";
-$username = "root";
+class Database {
+private $dsn = "mysql:host=localhost;dbname=myapp";
+private $username = "root";
 
-try{
-  $conn = new PDO($dsn, $username);
-  $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+private function connect(){
+  try{
+    $conn = new PDO($this->dsn, $this->username);
+    $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-  echo "Connection established successfully!";
-  displayData($conn);
-
-}catch(PDOException $e){
-  echo "Connection failed " . $e->getMessage(); 
-}
-
-function displayData($conn){
-  $statement = $conn->prepare('SELECT * FROM onlyPHP');
-  $statement->execute();
-  $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-  echo "<ul>";
-  foreach($results as $result){
-    echo "<li>". $result['title'] . "</li>";
+    echo "Connection established successfully!";
+    return $conn;
+  }catch(PDOException $e){
+    echo "Connection failed " . $e->getMessage(); 
+    return null;
   }
-  echo "</ul>";
 }
+
+public function fetchData() {
+      $conn = $this->connect();
+      if($conn){
+      $statement = $conn->prepare('SELECT * FROM onlyPHP');
+      $statement->execute();
+      $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+      return $results;
+      }else{
+        return '';
+      }
+}
+
+}
+
+
+$database = new Database();
+$results = $database->fetchData();
+
+echo "<pre>";
+var_dump($results);
+echo "</pre>";
+
+
