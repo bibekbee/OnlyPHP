ca8f7e52cb38d07b7251eaeb50b71b74642dd961
bibekbee <bibekbee23@gmail.com>
2024-05-30 16:29:02 +0545

    Validator class and cleanup

---
+++ Validator.php
@@ -0,0 +1,33 @@
+<?php
+
+class Validator{
+    private $input;
+    private $min;
+    private $max;
+
+    public function __construct($input, $min, $max){
+        $this->input = $input;
+        $this->min = $min;
+        $this->max = $max;
+    }
+
+    function validate($length){
+        if($this->input != '' && $length > $this->min && $length < $this->max){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    function error($length){
+          if($this->input == ''){
+           return 'Empty note cannot be submitted';
+          }else if($length < $this->min){
+           return 'Note too small need to be atleast 3 char';
+          }else if($length > $this->max){
+           return 'Note too big need to be less than 1000 char';
+          }else{
+            return '';
+          }
+    }
+}
--- controller/note-create.php
+++ controller/note-create.php
@@ -1,4 +1,5 @@
 <?php
+require 'Validator.php';
 $user_id = 1;
 $errors = [];
 $message = '';
@@ -7,22 +8,17 @@ $input = '';
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
    $input = trim($_POST['note']);
    $input_length = strlen($input);
-   if($input != '' && $input_length > 3 && $input_length < 1000){ 
-   $database->query("INSERT INTO notes(title, user_id) VALUES(:title, :user_id)", 
-   [':title' => htmlspecialchars($input), ':user_id' => $user_id]);
-   $message =  "done";
+  
+   $validator = new Validator($input, 3, 1000);
+
+   if($validator->validate($input_length)){ 
+      $database->query("INSERT INTO notes(title, user_id) VALUES(:title, :user_id)", 
+      [':title' => htmlspecialchars($input), ':user_id' => $user_id]);
+      $message =  "done";
    }
 
-   if($input == ''){
-     $errors['name'] = 'Empty note cannot be submitted';
-   }else if($input_length < 3){
-     $errors['name'] = 'Note too small need to be atleast 3 char';
-   }else if($input_length > 1000){
-     $errors['name'] = 'Note too big need to be less than 1000 char';
-   }else{
-     null;
-   }
-   
+   $errors['name'] = $validator->error($input_length);
+ 
 }
 
 
